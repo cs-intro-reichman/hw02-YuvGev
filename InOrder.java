ab922b0a8b855629662bcd532c70f63d8cce6993
YuvGev <yuval.geva01@post.runi.ac.il>
2023-12-31 12:38:37 +0200

    first push HW2

--- InOrder.java
+++ InOrder.java
@@ -5,5 +5,24 @@
 public class InOrder {
 	public static void main (String[] args) {
 		//// Write your code here
+	    int numGen1 = (int) (Math.random ()*10); 
+        System.out.print(numGen1);
+        
+        int i = 0;
+		do{
+			
+			int numGen2 = (int) (Math.random ()*10);
+
+			if ( numGen2 >= numGen1){
+
+				System.out.print(" " + numGen2);
+				numGen1 = numGen2;
+
+			} else{
+
+				i++;
+			}
+
+		} while(i == 0);
 	}
 }
